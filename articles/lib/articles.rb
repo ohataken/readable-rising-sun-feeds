2fe095c718d3950c6aef762354ec872b581fabe0
ohataken <11092425+ohataken@users.noreply.github.com>
2024-07-28 14:08:49 +0900

    bundle gem articles

---
+++ articles/lib/articles.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "articles/version"
+
+module Articles
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ articles/lib/articles/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Articles
+  VERSION = "0.1.0"
+end
