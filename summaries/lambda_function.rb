cad890f958365122804b53a6d8397dbaa5c95efc
Ken Ohata <11092425+ohataken@users.noreply.github.com>
2024-08-17 17:45:26 +0900

    feature: supports Cache-Control

--- summaries/lambda_function.rb
+++ summaries/lambda_function.rb
@@ -8,7 +8,16 @@ module LambdaFunction
       id = event["queryStringParameters"]["id"]
       summaries = Summaries::Summary.new(id)
       summaries.fill_descriptions!
-      summaries.document
+
+      {
+        isBase64Encoded: false,
+        statusCode: 200,
+        headers: {
+          "Cache-Control": "public, max-age=3600"
+        },
+        # "multiValueHeaders": { "headerName": ["headerValue", "headerValue2", ...], ... },
+        body: summaries.document.to_s
+      }
     end
   end
 end
