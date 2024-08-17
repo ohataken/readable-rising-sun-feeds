25dce4206001116808ff21bc6cf85a33b2b31cc7
Ken Ohata <11092425+ohataken@users.noreply.github.com>
2024-08-17 16:49:37 +0900

    feature: supports Cache-Control

--- articles/lambda_function.rb
+++ articles/lambda_function.rb
@@ -8,6 +8,16 @@ module LambdaFunction
       id = event["queryStringParameters"]["id"]
       article = Articles::Article.new(id)
       article.description
+
+      {
+        isBase64Encoded: false,
+        statusCode: 200,
+        headers: {
+          "Cache-Control": "public, max-age=31536000"
+        },
+        # "multiValueHeaders": { "headerName": ["headerValue", "headerValue2", ...], ... },
+        body: article.to_json
+      }
     end
   end
 end
