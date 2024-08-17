686162ea7880e0bb7a9aea6e7a38ba562670ad0e
Ken Ohata <11092425+ohataken@users.noreply.github.com>
2024-08-17 13:03:08 +0900

    feature: Lambda Function returns summaries with those article description

--- summaries/lambda_function.rb
+++ summaries/lambda_function.rb
@@ -1,10 +1,14 @@
+require "lib/summaries"
 require "io/console"
 require "stringio"
 
 module LambdaFunction
   class Handler
     def self.process(event:, context:)
-      "Hello from Lambda!"
+      id = event["queryStringParameters"]["id"]
+      summaries = Summaries::Summary.new(id)
+      summaries.fill_descriptions!
+      summaries.document
     end
   end
 end
